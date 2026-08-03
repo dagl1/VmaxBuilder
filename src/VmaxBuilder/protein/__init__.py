a80a33625e4db785eba63df3a26b987b1989b23c
dagl1 <jellebonthuis@hotmail.com>
2026-08-03 16:37:14 +0200

    refactor(protein): removed unused legacy protein implementation

--- src/VmaxBuilder/protein/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Generated: validation needed.
-
-Description:
-    Protein implementation exports for refactored VmaxBuilder pipeline.
-"""
-
-from VmaxBuilder.protein.proteomics_implementation import DefaultProteomicsImplementation
-from VmaxBuilder.protein.ptr_implementation import DefaultPTRImplementation
-from VmaxBuilder.protein.stage_implementation import DefaultProteinStageCoordinator
