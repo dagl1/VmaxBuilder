c04a40fe051a7eeafec92d4fd5a6c674f0ce0d1b
dagl1 <jellebonthuis@hotmail.com>
2026-08-13 16:55:19 +0200

    feat(kcat_stage): initial commit for kcat stage

---
+++ src/VmaxBuilder/stages/Kcat/main_substrate/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class MainSubstrateConfig:
+    main_substrate_selection_statistic: str = "max"
+    missing_prediction_strategy: str = "all"  # options: ["all", "per_compartment"]
+    missing_prediction_statistic: str = "median"  # options: ["mean", "median"]
